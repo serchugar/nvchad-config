c7ac392bfddea8b5434c377dfef27c75dcf6f6e3
serchugar <chugarsergio@gmail.com>
2025-09-30 17:26:54 +0200

    feat(ai, config): add copilot and keymap to fzf config files

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,3 +4,7 @@ local servers = { "clangd", "jdtls", "pylsp", "html", "cssls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
+
+vim.keymap.set("n", "<leader>ca", function()
+    vim.lsp.buf.code_action()
+end)
